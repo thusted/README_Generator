d8d520b09c95c5fa001578629e79e4f8365344ce
Tiana Husted <tiana.husted@gmail.com>
2020-07-28 16:29:49 -0700

    Updating generateMarkdown.js with code to format finished markdown document for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+## Description
+${data.description}
 
+## Table of Contents
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Licenses](#Licenses)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Licenses
+${data.licenses}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any further questions, feel free to contact me at ${data.github}@github.com or ${data.email}.
 `;
 }
 
