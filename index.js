8d70fdf200beb3904f2ac525765388c4eca4e57a
Tiana Husted <tiana.husted@gmail.com>
2020-07-28 16:08:24 -0700

    Updating index.js. Filling questions array with questions pertaining to title, description, installation, usage, licenses for project as well as github username and email

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide any installation instructions for your project."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide any usage information."
+    },
+    {
+        type: "input",
+        name: "licenseName",
+        message: "Please list the names of any licenses you would like to include."
+    },
+    {
+        type: "input",
+        name: "licenseURL",
+        message: "Please list the URL of the licenses you would like to include."
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
 ];
 
 // function to write README file
